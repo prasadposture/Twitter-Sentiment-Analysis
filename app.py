2a2f5cc53b974d989ba830aa48c03d5299a050a5
Prasad Rajesh Posture <95558446+prasadposture@users.noreply.github.com>
2023-07-03 12:14:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import re
+import string
+import nltk
+import joblib as jb
+import warnings
+warnings.filterwarnings("ignore")
+input_tweet=st.text_input('Enter the tweet')
+df = pd.DataFrame([{'tweet':input_tweet}])
+def remove_pattern(input_txt, pattern):
+    r = re.findall(pattern, input_txt)
+    for word in r:
+        input_txt = re.sub(word, "", input_txt)
+    return input_txt
+df['clean_tweet'] = np.vectorize(remove_pattern)(df['tweet'], "@[\w]*")
+df['clean_tweet'] = df['clean_tweet'].str.replace("[^a-zA-Z#]"," ")
+df['clean_tweet'] = df['clean_tweet'].apply(lambda x:" ".join([w for w in x.split() if len(w)>3]))
+tokenized_tweet = df['clean_tweet'].apply(lambda x: x.split())
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+tokenized_tweet = tokenized_tweet.apply(lambda sentence: [stemmer.stem(word) for word in sentence])
+for i in range(len(tokenized_tweet)):
+    tokenized_tweet[i]=" ".join(tokenized_tweet[i])
+df['clean_tweet']=tokenized_tweet
+tsa = jb.load('tsa.joblib')
+model = tsa['model']
+bow_vectorizer = tsa['bow_vectorizer']
+bow = bow_vectorizer.transform(df['clean_tweet'])
+pred = model.predict(bow)
+if pred==0:
+    st.write("the tweet is positive")
+else:
+    st.write("the tweet is negative")
